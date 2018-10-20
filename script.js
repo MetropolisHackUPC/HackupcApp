77ee647d6244d10ce8aa9e2676d178d25347fe56
Sergi Miralles <sergi.miralles@est.fib.upc.edu>
2018-10-20 14:49:40 +0200

    No se que estic fent aqui

---
+++ script.js
@@ -0,0 +1,134 @@
+var database = firebase.database();
+var url;
+// *****************************  PREGONS  ********************************************
+
+function BuscaPregons() {    
+    var ref = database.ref("prego/");
+    ref.on("value", function(snapshot) {
+      var arr = snapshot.val();
+      var arr2 = Object.keys(arr);
+
+      for (let i = 0; i < arr2.length; i++) {
+        var key = arr2[i];
+
+        ref = database.ref("prego/" + key);
+        ref.on("value", function(snapshot) {
+          nom = snapshot.val().titol;
+          des = snapshot.val().descripcio;
+          CreaPrego(nom,des);
+        });
+      }
+    });
+}
+
+function CreaPrego(nom, des) {
+  document.getElementById('linea').innerHTML += "";
+  document.getElementById('linea').innerHTML +=
+  `<div class="card mb-4 shadow-sm">
+   <div class="card-header">
+   <h4 class="my-0 font-weight-normal">${nom}</h4>
+   </div>
+   
+   <div class="card-body">
+    <li>${des}</li>
+    <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
+   </div>`;
+}
+
+// *****************************  INCIDENCIES  ********************************************
+
+function BuscaIncidents() {    
+  var ref = database.ref("incident/");
+  ref.on("value", function(snapshot) {
+    var arr = snapshot.val();
+    var arr2 = Object.keys(arr);
+
+    for (let i = 0; i < arr2.length; i++) {
+      var key = arr2[i];
+
+      ref = database.ref("incident/" + key);
+      ref.on("value", function(snapshot) {
+        nom = snapshot.val().titol;
+        des = snapshot.val().descripcio;
+        img = snapshot.val().imatge;
+        grau = snapshot.val().grau;
+        CreaIncidents(nom,des, img, grau);
+      });
+    }
+  });
+}
+
+function lengthChilds() {
+  var ref = database.ref();
+  ref.child('incident').on("value", function(snapshot) {
+    console.log(snapshot.numChildren());
+  });
+}
+
+function CreaNouIncident() {
+  nom = document.getElementById(toString(lengthChilds) + "TitolNou").value;
+  des = document.getElementById("DescripcioNou").value;
+  grau = document.getElementById("GrauNou").value;
+  img = url;
+
+  if (nom != "" && des != "") {
+    AfegirFirebaseIncident(nom, des, img, grau);
+    document.getElementById('linea').innerHTML = "";
+    BuscaPregons();
+  }
+}
+
+function CreaIncident(nom, des, img, grau) {
+    document.getElementById('linea').innerHTML += "";
+    document.getElementById('linea').innerHTML +=
+    `<div class="card mb-4 shadow-sm">
+      <div class="card-header">
+      <h4 class="my-0 font-weight-normal">${nom}</h4>
+     </div>
+    
+     <div class="card-body">
+        <li>${des}</li>
+        <input type="file" value="upload" id="fileButton">
+        <button type="button" class="btn btn-sm btn-outline-secondary">Delete</button>
+     </div>`;
+  }
+
+function AfegirFirebaseIncident(nom, des, img, grau) {
+  database.ref('incident/'+nom).set({
+    titol: nom,
+    descripcio: des,
+    imatge: img,
+    grau: grau
+  });
+}
+
+// *****************************  FIREBASE PHOTOs  ********************************************
+var fileButton = document.getElementById("fileButton");
+fileButton.addEventListener("change", function (e) {
+  //Get file
+  var file = e.target.files[0];
+  //Create a storage ref
+  var storageRef = firebase.storage().ref().child('Photos/'+'1.jpg');
+  //Upload file
+  storageRef.put(file);
+})
+/*
+document.getElementById(FormInci).addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  var x = document.getElementById("foto");
+  var file = x.target.files[0];
+  //Create a storage ref
+  var storageRef = firebase.storage().ref().child();
+  //Upload file
+  var lask = storageRef.put(file);
+}*/
+
+var Desc = document.getElementById("FD");
+Desc.addEventListener("click", function () {
+  var storageRef = firebase.storage().ref();
+  storageRef.child(hjkhkj).getDownloadURL().then(function(url) {
+    var img = document.getElementById("img");
+    img.src = url;
+  })
+})
